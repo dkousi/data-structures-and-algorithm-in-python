f8b413f89bc939e2c0685baa837db9668cb15fe7
dkousi <43299986+dkousi@users.noreply.github.com>
2019-06-06 14:35:41 +0530

    Create ass3-2*list1-inlist2.py

---
+++ ass3-2*list1-inlist2.py
@@ -0,0 +1,17 @@
+#DSA-Assgn-3
+
+def check_double(list1,list2):
+    new_list=[]
+    for i in list1:
+        j=i*2
+        if j in list2:
+            new_list.append(i)
+    return new_list
+
+#Provide different values for the variables and test your program
+list1=[11,8,23,7,25,15]
+list2=[6,33,50,31,46,78,16,34]
+print(check_double(list1, list2))
+
+
+output:[8, 23, 25]
